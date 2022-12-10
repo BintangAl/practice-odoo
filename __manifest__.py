c2548dfea21f479eae08d9d59fc1ee77791d8b06
Bintang Alsyahadat <bintangalsyahadat1@gmail.com>
2022-12-10 08:13:33 +0700

    Module Real Estate

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Admin",
+    'category': 'Sales',
+    'description': "",
+    # data files always loaded at installation
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
