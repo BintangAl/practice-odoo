180093b47fbbd95e9711893f50618e8e89f92413
Bintang Alsyahadat <bintangalsyahadat1@gmail.com>
2022-12-10 11:06:04 +0700

    Add Menu & Action

--- __manifest__.py
+++ __manifest__.py
@@ -9,6 +9,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_action.xml',
         'views/estate_menus.xml',
     ],
     # data files containing optionally loaded demonstration data
--- models/estate_property.py
+++ models/estate_property.py
@@ -4,13 +4,13 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
 
-    name = fields.Char(required=True)
+    name = fields.Char(string="Title", required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(string="Available From", copy=False, default=lambda self: fields.Date.today())
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -26,4 +26,17 @@ class EstateProperty(models.Model):
         ], 
         help="Orientation is used to indicate the position of the Park"
     )
+    status = fields.Selection(
+        selection = [
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled')
+        ],
+        default='new', 
+        copy=False,
+        required=True
+    )
+    active = fields.Boolean(default=True)
 
