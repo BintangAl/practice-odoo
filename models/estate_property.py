a06159c206a936448dbbf8d1e1730f12a4ba73c0
Bintang Alsyahadat <bintangalsyahadat1@gmail.com>
2022-12-11 09:15:35 +0700

    Chapter 9: Compute with Dependencies - Get Total Area of Living Area & Garden Area

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -44,4 +44,18 @@ class EstateProperty(models.Model):
     partner_id = fields.Many2one('res.partner',string='Buyer')
     tag_ids = fields.Many2many('estate.property.tag')
     offer_ids = fields.One2many('estate.property.offer', 'property_id')
+    total_area = fields.Float(string="Total Area (sqm)", compute="_compute_total_area")
+    best_price = fields.Float(compute="_compute_best_price")
 
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            try:
+                record.best_price = max(offer.price for offer in record.offer_ids) #get maximum price of offer
+            except:
+                record.best_price = 0
