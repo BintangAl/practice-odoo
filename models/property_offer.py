5bacd6adc25b1e6e8ee1ed302e3acac7a93db36c
Bintang Alsyahadat <bintangalsyahadat1@gmail.com>
2022-12-11 13:03:13 +0700

    Chapter 9: Compute with Inverse Function - Add Validity Deadline Offer

--- models/property_offer.py
+++ models/property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models
+import datetime
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -16,3 +17,14 @@ class EstatePropertyOffer(models.Model):
         string='Property',
         required=True,
     )
+    validity = fields.Integer(string="Validity (days)", default=7)
+    date_deadline = fields.Date(string="Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends('validity', 'create_date')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = datetime.datetime.now() + datetime.timedelta(days=record.validity)
+    
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date.date()).days
