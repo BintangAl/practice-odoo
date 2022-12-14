56634a18af6318592df6f75316c66db8258f6099
Bintang Alsyahadat <bintangalsyahadat1@gmail.com>
2022-12-14 09:26:13 +0700

    Chapter 14: Inherit Model in another module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Estate Account",
+    'version': '1.0',
+    'depends': ['base', 'estate', 'account'],
+    'author': "Admin",
+    'category': 'Sales',
+    'description': "",
+    # data files always loaded at installation
+    'data': [],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,31 @@
+from odoo import Command, fields, models
+
+class EstateProperty(models.Model):
+    _inherit = 'estate.property'
+
+    def action_property_sold(self):
+        # journal = self.env["account.journal"].search([("type", "=", "sale")], limit=1)
+        journal = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
+
+        for record in self:
+            self.env['account.move'].create(
+                {
+                    "partner_id" : record.partner_id.id,
+                    "move_type" : "out_invoice",
+                    "journal_id" : journal.id,
+                    "invoice_line_ids" : [
+                        Command.create({
+                            "name" : record.name,
+                            "quantity" : 1,
+                            "price_unit" : record.selling_price * 6 / 100
+                        }),
+                        Command.create({
+                            "name" : "Administrative fees",
+                            "quantity" : 1,
+                            "price_unit" : 100
+                        })
+                    ]
+                }
+            )
+
+        return super().action_property_sold()
