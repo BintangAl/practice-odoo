617a867814317d721b4523785f786ea327e8067f
Bintang Alsyahadat <bintangalsyahadat1@gmail.com>
2022-12-22 14:29:57 +0700

    Advanced B: Set Bypassing Security

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -4,11 +4,14 @@ class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
     def action_property_sold(self):
+        self.env.user.check_access_rights('write')
+        self.env.user.check_access_rule('write')
+
         # journal = self.env["account.journal"].search([("type", "=", "sale")], limit=1)
-        journal = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
+        journal = self.env['account.move'].sudo().with_context(default_move_type='out_invoice')._get_default_journal()
 
         for record in self:
-            self.env['account.move'].create(
+            self.env['account.move'].sudo().create(
                 {
                     "partner_id" : record.partner_id.id,
                     "move_type" : "out_invoice",
